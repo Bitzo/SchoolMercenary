f08a94f4f061bccd11837b8b6a7d1d94db07c306
bitzo <bitzo@qq.com>
2018-01-22 17:51:05 +0800

    add: 字典表模型

---
+++ db/models/dicModel.js
@@ -0,0 +1,48 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../db');
+
+const dictionary = sequelize.define('dictionary', {
+  id: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  level: {
+    type: Sequelize.INTEGER(4),
+    allowNull: false,
+    defaultValue: '1',
+  },
+  category: {
+    type: Sequelize.STRING(10),
+    allowNull: false,
+  },
+  code: {
+    type: Sequelize.STRING(20),
+    allowNull: false,
+    unique: true,
+  },
+  value: {
+    type: Sequelize.STRING(20),
+    allowNull: false,
+  },
+  createTime: {
+    type: Sequelize.DATEONLY,
+    allowNull: false,
+  },
+  parent: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+  },
+  isActive: {
+    type: Sequelize.INTEGER(1),
+    allowNull: false,
+    defaultValue: '1',
+  },
+}, {
+  createdAt: 'createTime',
+  updatedAt: false,
+  tableName: 'dictionary',
+});
+
+module.exports = dictionary;
